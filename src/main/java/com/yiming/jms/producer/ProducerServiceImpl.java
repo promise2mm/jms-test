2a0cd53967c0b3a7d668ee79c010018b52950480
yiming <yiming@anve.com>
2018-01-24 14:40:13 +0800

    java8

--- src/main/java/com/yiming/jms/producer/ProducerServiceImpl.java
+++ src/main/java/com/yiming/jms/producer/ProducerServiceImpl.java
@@ -2,12 +2,7 @@ package com.yiming.jms.producer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
 
 /**
  * Created by yiming on 2017/7/16.
@@ -18,11 +13,8 @@ public class ProducerServiceImpl implements ProducerService {
     @Autowired
     private JmsTemplate jmsTemplate;
 
+    @Override
     public void sendMessage(final String message) {
-        jmsTemplate.send(new MessageCreator() {
-            public Message createMessage(Session session) throws JMSException {
-                return session.createTextMessage(message);
-            }
-        });
+        jmsTemplate.send(session -> session.createTextMessage(message));
     }
 }
